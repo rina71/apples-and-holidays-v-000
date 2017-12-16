10196198ca750c496652467258d0a6ae3b97e6c5
Yurina Shin <yurinas@gmail.com>
2017-12-16 19:59:02 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,9 +64,9 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  holiday_hash.each{|season,holiday| puts "#{season.capitalize}:"
+  holiday_hash.each{|season,holidays| puts "#{season.capitalize}:"
   count = 0
-   holiday.each{|holiday, supply| puts "  #{holiday.capitalize}: #{supply.flatten}"
+   holidays.each{|holiday, supply| puts "  #{holiday.capitalize}: #{supply.flatten}"
   }}
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
