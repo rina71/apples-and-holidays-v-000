13b1ccfc62419c984c25f0ce143b34276e666daa
Yurina Shin <yurinas@gmail.com>
2017-12-16 19:47:37 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,9 +79,10 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-holiday_hash.each{|season,holidays|
+holy = []
+holiday_hash.each{|season,holidays| 
   holidays.collect{|holiday, supply| if supply.include?("BBQ") == true
-    holy = [holiday]
+    holy = holiday
 
   end
 holy
